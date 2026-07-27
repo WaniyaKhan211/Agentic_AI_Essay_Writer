17c96e8e268c4d4579f0b72432b2ef8305882613
WaniyaKhan211 <waniyakhan2111@gmail.com>
2026-07-27 11:35:08 +0500

    Write Configurations

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# API KEYS
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+HF_API_KEY=os.getenv("HF_API_KEY")
+EXA_API_KEY=os.getenv("EXA_API_KEY")
+
+#Models
+GROQ_MODEL= "openai-gpt-oss-120"
+HF_MODEL= "black-forest-labs/FLUX.1-Krea-dev"
+
+TEMPERATURE =0.7
+MAX_TOKENS = 2000
+
