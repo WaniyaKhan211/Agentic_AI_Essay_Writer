71308f7d50733dd9d78735905d34165a04ee4a8a
WaniyaKhan211 <waniyakhan2111@gmail.com>
2026-08-15 15:05:48 +0500

    Preparation for deployment

--- backend/app.py
+++ backend/app.py
@@ -38,9 +38,20 @@ app = FastAPI(
     title="Agentic AI Essay Writer API"
 )
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+import os
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[FRONTEND_URL, "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
